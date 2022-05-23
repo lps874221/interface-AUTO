3530d63bcd385eb91109e82383a056bb6b585861
lps874221 <2210384396@qq.com>
2022-05-23 11:28:26 +0800

    Update main1.py

--- jiabiaoyongliheqianzhijiekou/main1.py
+++ jiabiaoyongliheqianzhijiekou/main1.py
@@ -1,15 +1,10 @@
 import unittest
 from HTMLTestRunnerNew import HTMLTestRunner
 from ningmengban2019.day10.jiabiaoyongliheqianzhijiekou.Common.handle_path import cases_dir,reports_dir
-#实例化套件对象
+
 s = unittest.TestSuite()
-#TestLoader的用法
-#1、实例化TestLoader对象
-# 2、使用discover去找到一个目录下的所有测试用例
-#3、使用s
 loader = unittest.TestLoader()
-s.addTests(loader.discover(cases_dir))#不光是同级目录，里面子目录的用例也可以收集到
-# #运行
+s.addTests(loader.discover(cases_dir))
 # runner = unittest.TextTestRunner()
 # runner.run(s)
 
