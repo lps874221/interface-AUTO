8d3a1685d899fd72de4cecb26f7466a6f68284dd
lps874221 <2210384396@qq.com>
2022-05-23 10:51:04 +0800

    Update handle_phone.py

--- jiabiaoyongliheqianzhijiekou/Common/handle_phone.py
+++ jiabiaoyongliheqianzhijiekou/Common/handle_phone.py
@@ -1,23 +1,4 @@
-"""
-======================
-Author: 柠檬班-小简
-Time: 2020/7/3 22:02
-Project: day5
-Company: 湖南零檬信息技术有限公司
-======================
-"""
-"""
-======================
-Author: 柠檬班-小简
-Time: 2020/7/3 21:46
-Project: day5
-Company: 湖南零檬信息技术有限公司
-======================
-"""
-"""
-1、随机生成11位手机号  前3位+8位
-2、进行数据校验
-"""
+
 
 import random
 from ningmengban2019.day10.jiabiaoyongliheqianzhijiekou.Common.handle_db import HandleDB
@@ -37,22 +18,16 @@ def get_new_phone():
         phone = __generator_phone()
         # 2校验，有
         count = db.get_count('select * from member where mobile_phone="{}"'.format(phone))
-        if count == 0: # 如果手机号码没有在数据库查到。表示是未注册的号码。
+        if count == 0: 
             db.close()
             return phone
 
 
 def get_old_phone():
-    """
-    从配置文件获取指定的用户名和密码
-    确保此帐号，在系统当中是注册了的。
-    返回：用户名和密码。
-    """
+   
     from ningmengban2019.day10.jiabiaoyongliheqianzhijiekou.Common.handle_config import conf
     user = conf.get("general_user","user")
     passwd = conf.get("general_user","passwd")
-    # 如果数据库查找到user，就直接返回。如果没有，则调用注册接口注册一个。
-    # 不管注册与否，直接调用注册接口。如果已有号码，则不会注册，但是没有的话，则新注册一个
     send_requests("POST","member/register",{"mobile_phone":user,"pwd":passwd})
     return user,passwd
 
@@ -65,6 +40,6 @@ def __generator_phone():
     return phone
 
 
-# print(get_new_phone())
+
 
 
